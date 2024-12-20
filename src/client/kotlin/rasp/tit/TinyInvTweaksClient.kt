685161d17bcdf6e54e5da1fd4dc4cdde0a14f5f9
raspirin <f1193968991@126.com>
2024-12-20 18:43:58 +0800

    feat: init commit

---
+++ src/client/kotlin/rasp/tit/TinyInvTweaksClient.kt
@@ -0,0 +1,9 @@
+package rasp.tit
+
+import net.fabricmc.api.ClientModInitializer
+
+object TinyInvTweaksClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

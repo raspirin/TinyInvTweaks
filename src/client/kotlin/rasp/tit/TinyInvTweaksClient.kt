1d54cf287598eaa54e55df12f076081b0fb6bd39
raspirin <f1193968991@126.com>
2024-12-21 18:41:10 +0800

    feat: empty impl for mixins to dispatch to

--- src/client/kotlin/rasp/tit/TinyInvTweaksClient.kt
+++ src/client/kotlin/rasp/tit/TinyInvTweaksClient.kt
@@ -1,9 +1,16 @@
 package rasp.tit
 
 import net.fabricmc.api.ClientModInitializer
+import rasp.tit.action.InvTweaksAction
 
 object TinyInvTweaksClient : ClientModInitializer {
+	private val config = TinyInvTweaksClientConfig()
+
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+
+	public fun executeAction(action: InvTweaksAction) {
+
 	}
 }
--- src/main/kotlin/rasp/tit/TinyInvTweaks.kt
+++ src/main/kotlin/rasp/tit/TinyInvTweaks.kt
@@ -4,12 +4,10 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object TinyInvTweaks : ModInitializer {
-    private val logger = LoggerFactory.getLogger("tiny-inv-tweaks")
-
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+		TinyInvTweaksConst.logger.info("Hello Fabric world!")
 	}
 }

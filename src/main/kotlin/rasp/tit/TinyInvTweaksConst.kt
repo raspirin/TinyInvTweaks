9719da9d111bc35fb29113b1d0e70a6755c007ae
raspirin <f1193968991@126.com>
2024-12-21 18:39:37 +0800

    feat: mod const

---
+++ src/main/kotlin/rasp/tit/TinyInvTweaksConst.kt
@@ -0,0 +1,8 @@
+package rasp.tit
+
+import org.slf4j.LoggerFactory
+
+object TinyInvTweaksConst {
+    public val modid = "tiny-inv-tweaks"
+    public val logger = LoggerFactory.getLogger(modid)
+}

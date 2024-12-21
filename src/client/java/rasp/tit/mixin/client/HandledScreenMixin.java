71c16bd73d75068c6b1fcbae1ed0b0a37d733716
raspirin <f1193968991@126.com>
2024-12-21 18:40:43 +0800

    feat: basic mixin impl

--- src/client/java/rasp/tit/mixin/client/HandledScreenMixin.java
+++ src/client/java/rasp/tit/mixin/client/HandledScreenMixin.java
@@ -5,12 +5,14 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rasp.tit.TinyInvTweaksClient;
+import rasp.tit.TinyInvTweaksConst;
+import rasp.tit.action.InvTweaksAction;
+import rasp.tit.action.InvTweaksActionFactory;
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen implements ScreenHandlerProvider<T> {
@@ -20,13 +22,15 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
 
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
     private void mouseClickedHook(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
-        Logger logger = LoggerFactory.getLogger("tiny-inv-tweaks");
-        logger.info("mouse clicked hit!");
+        TinyInvTweaksConst.INSTANCE.getLogger().info("mouse clicked hit!");
+        InvTweaksAction action = InvTweaksActionFactory.INSTANCE.fromMouseEvent();
+        TinyInvTweaksClient.INSTANCE.executeAction(action);
     }
 
     @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
     private void keyPressedHook(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        Logger logger = LoggerFactory.getLogger("tiny-inv-tweaks");
-        logger.info("key pressed hit!");
+        TinyInvTweaksConst.INSTANCE.getLogger().info("key pressed hit!");
+        InvTweaksAction action = InvTweaksActionFactory.INSTANCE.fromKeyEvent();
+        TinyInvTweaksClient.INSTANCE.executeAction(action);
     }
 }
